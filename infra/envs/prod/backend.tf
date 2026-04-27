b71e52c7ada9b18477c590df50dfa980d90146d4
azad <azad.learn@learn.com>
2026-04-27 17:20:41 +0530

    qa env tfstate file

--- infra/envs/prod/backend.tf
+++ infra/envs/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateacct123"
+    resource_group_name  = "finops-poc-rg"
+    storage_account_name = "stfinopsdataxxx"
     container_name       = "tfstate"
     key                  = "prd.tfstate"
   }
